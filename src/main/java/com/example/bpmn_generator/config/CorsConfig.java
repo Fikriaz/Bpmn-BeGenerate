86f994e99f34304241881334de7cfd617c65e878
Fikriaz <fikri.achmadfauzi77@gmail.com>
2025-10-02 20:08:04 +0700

    add cors

---
+++ src/main/java/com/example/bpmn_generator/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.bpmn_generator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",   // FE lokal
+                                "https://automation-test.up.railway.app", // optional
+                                "https://<nama-project-vercel>.vercel.app" // nanti kalau deploy FE
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
